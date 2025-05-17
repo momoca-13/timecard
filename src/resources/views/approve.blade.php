4782e4ed1c3ef5f78f6eeba7a63fad761e0efd37
momoca <tell.me.something.mmk@gmail.com>
2025-05-18 01:39:11 +0900

    viewとcontroller

---
+++ src/resources/views/approve.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>修正申請承認画面（管理者）</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ src/resources/views/login.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン画面（一般ユーザー）</title>
+</head>
+<body>
+    
+</body>
+</html>
